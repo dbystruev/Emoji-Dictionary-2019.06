a0791c76f9864202b978e9dce3e27b0e1ca96e40
Denis Bystruev <dbystruev@me.com>
2019-06-07 22:52:33 +0300

    Simplified App Delegate

---
+++ Emoji Dictionary/App/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  Emoji Dictionary
+//
+//  Created by Denis Bystruev on 07.06.2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
